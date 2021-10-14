6dc7a59b26fe56e2ad169441b65a1d99a0ddf79c
yanqi.zyq <yanqi.zyq@antfin.com>
2021-10-14 11:45:20 +0800

    init

---
+++ index.js
@@ -0,0 +1,39 @@
+const ChatBot = require("dingtalk-robot-sender");
+
+function main(token, mobile) {
+  const defaultOptions = {
+    baseUrl: "https://oapi.dingtalk.com/robot/send",
+    accessToken: token,
+  };
+
+  const texts = [
+    "【提醒】记得喝水哦~",
+    "【提醒】每天八杯水，健康一整天~",
+    "【提醒】记得休息一下",
+    "【提醒】别太累了，起来运动运动",
+    "【提醒】专注工作，提高效率",
+    "【提醒】没啥事，关心一下",
+    "【提醒】重要的事情说三遍，喝水，喝水，喝水",
+    "【提醒】坚持一下，马上就要下班啦！",
+  ];
+
+  var content = texts[Math.floor(Math.random() * texts.length)];
+  const robot = new ChatBot(defaultOptions);
+
+  const at = {
+    atMobiles: [mobile],
+    isAtAll: false,
+  };
+  // 快速发送文本消息
+  robot.text(content, at).then((res) => {
+    console.log("发送消息成功");
+  });
+}
+
+if (!module.parent) {
+  let token = args[1];
+  let mobile = args[2];
+  main(token, mobile);
+} else {
+  // we were require()d from somewhere else
+}
